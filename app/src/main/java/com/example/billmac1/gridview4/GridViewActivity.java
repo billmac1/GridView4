7e66b355087048b95ce02c2b39bc421353e24abe
Bill McCarthy <billmacnyc@gmail.com>
2016-01-14 13:56:35 -0500

    Style Action bar

--- app/src/main/java/com/example/billmac1/gridview4/GridViewActivity.java
+++ app/src/main/java/com/example/billmac1/gridview4/GridViewActivity.java
@@ -25,8 +25,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -37,6 +35,8 @@ public class GridViewActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
     private GridViewAdapter mGridAdapter;
     private ArrayList<GridItem> mGridData;
+
+
 
    // private String FEED_URL = "http://javatechig.com/?json=get_recent_posts&count=45";
    final String FORECAST_BASE_URL =
@@ -202,149 +202,6 @@ public class GridViewActivity extends AppCompatActivity {
         }
     }
 
-    public class FetchMovieDataTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchMovieDataTask.class.getSimpleName();
-
-
-        private String[] getPostersFromJson(String movieJsonStr)
-                throws JSONException {
-
-            // These are the names of the JSON objects that need to be extracted.
-            final String OWM_LIST = "results";
-            final String OWM_PATH = "poster_path";
-            final String OWM_ID = "id";
-
-            JSONObject movieJson = new JSONObject(movieJsonStr);
-            JSONArray movieArray = movieJson.getJSONArray(OWM_LIST);
-
-            int movieArraySize =  movieArray.length();
-            String[] resultStrs = new String[ movieArraySize];  //fix later
-
-
-            for(int i = 0; i < movieArraySize; i++) {
-                String postPath;
-                // Get the JSON object representing the movie
-                JSONObject movieData = movieArray.getJSONObject(i);
-                // posterPath is in a child array
-                //   JSONObject movieObject = movieData.getJSONArray(movieJsonStr).getJSONObject(0);
-                postPath = movieData.getString(OWM_PATH);
-                resultStrs[i] = postPath;
-            }
-
-            for (String s : resultStrs) {
-                Log.v(LOG_TAG, "Movie jpg: " + s);
-            }
-
-            return resultStrs;
-
-        }
-
-
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            // Will contain the raw JSON response as a string.
-            String movieJsonStr = null;
-
-            try{
-                // Construct the URL for the MovieDB query
-                final String FORECAST_BASE_URL =
-                        "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
-                final String API_KEY = "api_key";
-
-                Uri builder = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(API_KEY, BuildConfig.MOVIE_DB_API_KEY)
-                        .build();
-
-                URL url = new URL(builder.toString());
-
-                //   Log.v(LOG_TAG,"Built URI" + builder.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line).append("\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                movieJsonStr = buffer.toString();
-                //.v(LOG_TAG,"Forecast JSON String: " + forecastJsonStr);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the movie link, there's no point in attempting
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        //  Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try{
-                return getPostersFromJson(movieJsonStr);
-            }
-            catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            return null;
-
-        }
-
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            // Download complete. Let us update UI
-            if (result != null) {
-                mGridAdapter.setGridData(mGridData);
-            } else {
-                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
-            }
-            mProgressBar.setVisibility(View.GONE);
-        }
-    }
-  //      @Override
-  //      protected void onPostExecute(String[] result) {
-  //          if (result != null) {
-         //       mGridAdapter.clear();
-          //      mGridAdapter.addAll(result);    //works from KitKat and above
-           //     for (String dayForecastStr : result) {
-           //             mForecastAdapter.add(dayForecastStr);
-           //     }
-        //    }
-        //}
 
 
 
